3c5f303f4654d363572a7c029d23a51b12b83237
Daniel Sung <daniel.sung.2005@gmail.com>
2025-06-20 21:03:39 -0500

    feat: Update project structure and add authentication screens
    - Changed main entry point in package.json to "expo-router/entry".
    - Added AuthLayout component for authentication screens.
    - Implemented Login and Register components with form submission handling.
    - Created RootLayout for managing navigation and status bar.
    - Added About and Contact pages with navigation links.
    - Developed Home page with links to Login and Register.
    - Included themed components: ThemedButton, ThemedCard, ThemedLogo, ThemedText, ThemedView, and Spacer for consistent styling.
    - Introduced Colors.js for managing light and dark themes.
    - Added logo images for light and dark themes.

---
+++ app/_layout.jsx
@@ -0,0 +1,32 @@
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
+import { Colors } from "../constants/Colors"
+
+const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light
+  console.log(colorScheme);
+  return (
+    <>
+        <StatusBar value="auto" />
+        <Stack screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground},
+            headerTintColor: theme.title
+        }}>
+            <Stack.Screen name="(auth)" options={{ headerShown: false}} />
+
+
+            <Stack.Screen name="index" options={{
+                title: 'Home'
+            }}/>
+
+        </Stack>
+    
+    </>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
